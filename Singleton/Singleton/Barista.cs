30a764b32ea0fb7203c0b0658c5e8b10df57d4af
DVA <83889766+VikaDemkina@users.noreply.github.com>
2023-12-19 11:34:41 +0300

    Add files via upload

---
+++ Singleton/Singleton/Barista.cs
@@ -0,0 +1,28 @@
+﻿public class Barista
+{
+    private static Barista instance;
+
+    private Barista()
+    {
+        // Код инициализации и настройки баристы
+    }
+
+    public static Barista GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Barista();
+        }
+        return instance;
+    }
+
+    public void MakeCoffee(string coffeeType)
+    {
+        Console.WriteLine("Бариста готовит кофе " + coffeeType);
+    }
+
+    public void ServeCustomer(string customerName)
+    {
+        Console.WriteLine("Бариста обслуживает посетителя " + customerName);
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Получение экземпляра баристы
+        Barista barista = Barista.GetInstance();
+
+        // Вызов методов и функциональности баристы
+        barista.MakeCoffee("Фисташковый Раф");
+        barista.ServeCustomer("Виктория");
+    }
+}
